bdb38b46123407ff0c400596d52a7dc30812cc62
Yen <Yen50414@gmail.com>
2017-01-20 17:17:48 -0500

    Refactored roman numeral symbols into list

--- src/main/java/com/moa/romannumerals/RNSwitch.java
+++ src/main/java/com/moa/romannumerals/RNSwitch.java
@@ -1,62 +1,100 @@
 
 package com.moa.romannumerals;
 
+import java.util.ArrayList;
+
 public class RNSwitch {
+	
+	private ArrayList<String> symbolList;
+	
+	public RNSwitch() {
+		symbolList = new ArrayList<String>();
+		symbolList.add("M");
+		symbolList.add("CM");
+		symbolList.add("D");
+		symbolList.add("CD");
+		symbolList.add("C");
+		symbolList.add("XC");
+		symbolList.add("L");
+		symbolList.add("XL");
+		symbolList.add("X");
+		symbolList.add("IX");
+		symbolList.add("V");
+		symbolList.add("IV");
+		symbolList.add("I");
+	}
 
 	public String arabicToRoman(int arabic) {
 		
 		if (arabic <= 0 || arabic >= 4000)
 			return "Not a valid number";
 		
+		int symbolIndex = 0;
 		String roman = "";
 		
 		while (arabic >= 1000) {
-			roman += "M";
+			roman += symbolList.get(symbolIndex);
 			arabic -= 1000;
 		}
-		if (arabic >= 900) {
-			roman += "CM";
+		symbolIndex++;
+		while (arabic >= 900) {
+			roman += symbolList.get(symbolIndex);
 			arabic -= 900;
-		} else if (arabic >= 500) {
-			roman += "D";
+		}
+		symbolIndex++;
+		while (arabic >= 500) {
+			roman += symbolList.get(symbolIndex);
 			arabic -= 500;
-		} else if (arabic >= 400) {
-			roman += "CD";
+			
+		}
+		symbolIndex++;
+		while (arabic >= 400) {
+			roman += symbolList.get(symbolIndex);
 			arabic -= 400;
 		}
-		
+		symbolIndex++;
 		while (arabic >= 100) {
-			roman += "C";
+			roman += symbolList.get(symbolIndex);
 			arabic -= 100;
 		}
-		if (arabic >= 90) {
-			roman += "XC";
+		symbolIndex++;
+		while (arabic >= 90) {
+			roman += symbolList.get(symbolIndex);
 			arabic -= 90;
-		} else if (arabic >= 50) {
-			roman += "L";
+		}
+		symbolIndex++;
+		while (arabic >= 50) {
+			roman += symbolList.get(symbolIndex);
 			arabic -= 50;
-		} else if (arabic >= 40) {
-			roman += "XL";
+		}
+		symbolIndex++;
+		while (arabic >= 40) {
+			roman += symbolList.get(symbolIndex);
 			arabic -= 40;
 		}
-		
+		symbolIndex++;
 		while (arabic >= 10) {
-			roman += "X";
+			roman += symbolList.get(symbolIndex);
 			arabic -= 10;
 		}
-		if (arabic == 9) {
-			roman += "IX";
+		symbolIndex++;
+		while (arabic == 9) {
+			roman += symbolList.get(symbolIndex);
 			arabic -= 9;
-		} else if (arabic >= 5) {
-			roman += "V";
-			arabic -= 5;
-		} else if (arabic == 4) {
-				roman += "IV";
-				arabic -= 4;
 		}
-		
+		symbolIndex++;
+		while (arabic >= 5) {
+			roman += symbolList.get(symbolIndex);
+			arabic -= 5;
+		}
+		symbolIndex++;
+		while (arabic == 4) {
+			roman += symbolList.get(symbolIndex);
+			arabic -= 4;
+		}
+		symbolIndex++;
 		while (arabic > 0) {
-			roman += "I";
+			roman += symbolList.get(symbolIndex);
 			arabic--;
 		}
 		
