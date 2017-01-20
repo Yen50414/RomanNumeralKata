88f32c7d0539076ac0d8d1a04040bb9f479a55a1
Yen <Yen50414@gmail.com>
2017-01-19 21:01:11 -0500

    Fixed some formatting

--- src/main/java/com/moa/romannumerals/RNSwitch.java
+++ src/main/java/com/moa/romannumerals/RNSwitch.java
@@ -14,15 +14,14 @@ public class RNSwitch {
 		if (arabic >= 900) {
 			roman += "CM";
 			arabic -= 900;
-		}
-		while (arabic >= 500) {
+		} else if (arabic >= 500) {
 			roman += "D";
 			arabic -= 500;
-		}
-		if (arabic >= 400) {
+		} else if (arabic >= 400) {
 			roman += "CD";
 			arabic -= 400;
 		}
+		
 		while (arabic >= 100) {
 			roman += "C";
 			arabic -= 100;
@@ -30,15 +29,14 @@ public class RNSwitch {
 		if (arabic >= 90) {
 			roman += "XC";
 			arabic -= 90;
-		}
-		else if (arabic >= 50) {
+		} else if (arabic >= 50) {
 			roman += "L";
 			arabic -= 50;
-		}
-		else if (arabic >= 40) {
+		} else if (arabic >= 40) {
 			roman += "XL";
 			arabic -= 40;
 		}
+		
 		while (arabic >= 10) {
 			roman += "X";
 			arabic -= 10;
@@ -46,15 +44,14 @@ public class RNSwitch {
 		if (arabic == 9) {
 			roman += "IX";
 			arabic -= 9;
-		}
-		else if (arabic >= 5) {
+		} else if (arabic >= 5) {
 			roman += "V";
 			arabic -= 5;
-		}
-		else if (arabic == 4) {
+		} else if (arabic == 4) {
 				roman += "IV";
 				arabic -= 4;
 		}
+		
 		while (arabic > 0) {
 			roman += "I";
 			arabic--;
