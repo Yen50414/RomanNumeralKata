6230935a92717db66ee1be0b888329d04ac0d3ed
Yen <Yen50414@gmail.com>
2017-01-20 19:03:24 -0500

    Wrote main class to interact with users.

--- src/main/java/com/moa/romannumerals/RNSwitchMain.java
+++ src/main/java/com/moa/romannumerals/RNSwitchMain.java
@@ -1,10 +1,23 @@
 package com.moa.romannumerals;
 
+import java.util.Scanner;
+
 public class RNSwitchMain {
 	
+	private static RNSwitch rnSwitch;
+	
+	public static boolean isNumber(String str)
+	{
+	    for (char c : str.toCharArray())
+	    {
+	        if (!Character.isDigit(c)) {
+	        	return false;
+	        }
+	    }
+	    return true;
+	}
+	
 	private static void printAll() {
-
-		RNSwitch rnSwitch = new RNSwitch();
 		int maxValue = 4000;
 		
 		for (int i = 0; i <= maxValue; i++) {
@@ -14,8 +27,24 @@ public class RNSwitchMain {
 	}
 
 	public static void main(String[] args) {
+		rnSwitch = new RNSwitch();
 		
-		printAll();
+		try (Scanner scanner = new Scanner(System.in)) {
+			String input = "";
+			while (true) {
+				System.out.print("Enter a number or PRINT: ");
+				input = scanner.next();
+				if (input.equalsIgnoreCase("q")) {
+					System.out.println("Program Terminated.");
+					break;
+				} else if (input.equalsIgnoreCase("PRINT")) {
+					printAll();
+				} else if (isNumber(input)) {
+					System.out.println(input + " = " + rnSwitch.arabicToRoman(Integer.parseInt(input)));
+				} else {
+					System.out.println("Unaccepted Input.");
+				}
+			}
+		}
 	}
-
 }
